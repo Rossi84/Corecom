e090400080393950429a16439f929d0bc775b932
Federica Bertozzi <f.bertozzi@inera.it>
2021-01-11 16:46:49 +0100

    Negoziazione

--- conf.py
+++ conf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # -- PROJECT Variables ----------------------------------------------------
-settings_project_name = "CW"
+settings_project_name = "Manuale Corecom"
 settings_copyright_copyleft = "AGCOM"
 settings_editor_name = "Agcom - Autorita per le garanzie nelle comunicazioni"
 settings_doc_version = '1'
